4c169616007c830d4c287fb38f7a44eedc0b2caa
Platinum <platinumemirate@gmail.com>
2021-09-10 19:04:14 +0100

    Make Loadr Local

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import loaderImg from '../resources/ReactLogoLoader.png'
 
 const Loader = ({isLoading}) => {
   if(!isLoading) return null;
   return (
     <div id="loader" className="d-flex justify-content-center align-items-center flex-column">
-      <img src="https://react-pdf.org/images/logo.png" alt="loader" className="mb-5 App-logo" />
+      <img src={loaderImg} alt="loader" className="mb-5 App-logo" />
       <p>Loading...</p>
     </div>
   )
