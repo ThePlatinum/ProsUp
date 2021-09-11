06e97ec81ee604a48dd616285896c3f0d50c041c
Platinum <platinumemirate@gmail.com>
2021-09-11 01:00:47 +0100

    htaccess

--- api/ones.php
+++ api/ones.php
@@ -27,6 +27,6 @@
     }
     
     header('Content-Type: application/json');
-    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Access-Control-Allow-Origin: http://localhost:3000/');
     echo json_encode($projects);
 ?>
