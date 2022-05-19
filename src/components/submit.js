aa23a3a89ca5081e951b4a0b7839cb76fff51a48
Platinum <platinumemirate@gmail.com>
2022-05-19 22:24:25 +0100

    brand color change a15318 to 002198

--- src/components/submit.js
+++ src/components/submit.js
@@ -28,33 +28,33 @@ function Submit() {
     const [hasErrorFile, sethasErrorFile] = useState(true)
     const [fillAll, setFillAll] = useState('')
 
-    const sendSubmission = ()=>{
+    const sendSubmission = () => {
 
-        if(!hasErrorName && !hasErrorMail && !hasErrorNote && !hasErrorFile){
+        if (!hasErrorName && !hasErrorMail && !hasErrorNote && !hasErrorFile) {
             toggleModal()
             setFillAll('')
 
             const User_ID = 'user_ngIsUTPsb5AEBQ3b49qFi'
             const Template_ID = 'template_39xr60v'
 
-            emailjs.send("service_ozw18ud",Template_ID,{
+            emailjs.send("service_ozw18ud", Template_ID, {
                 message: `${note}`,
                 name: `${firstName}, ${lastName} ${otherName}`,
                 email: `${mail}`,
                 type: `${docType}`,
                 file: `${selectFileName}`,
-                }, User_ID)
-            .then(result => {
-                let status = result.status;
-                if (status === 200){
-                    setLoading('Your Submission has been successfull, \n You should get a mail from us, PLEASE CHECK YOUR SPAM and add us to your mailing list. \n We will contact you soon');
-                    window.location.reload(false);
-                }
-                else{
-                setLoading('Unable to complete your sumission, please try again.');
-                }
-            })
-        }else{
+            }, User_ID)
+                .then(result => {
+                    let status = result.status;
+                    if (status === 200) {
+                        setLoading('Your Submission has been successfull, \n You should get a mail from us, PLEASE CHECK YOUR SPAM and add us to your mailing list. \n We will contact you soon');
+                        window.location.reload(false);
+                    }
+                    else {
+                        setLoading('Unable to complete your sumission, please try again.');
+                    }
+                })
+        } else {
             setFillAll('Please fill all Required (* - marked) Fields')
         }
         /* const submisson = new FormData
@@ -78,128 +78,130 @@ function Submit() {
             .then(data => console.log(data)); */
     }
 
-  const [dropdownOpen, setdropdownOpen] = useState(false);
-  const toggle = () => setdropdownOpen(!dropdownOpen);
-  const [nameError, setnameError] = useState('')
-  const [mailError, setmailError] = useState('')
-  const [noteError, setnoteError] = useState('')
+    const [dropdownOpen, setdropdownOpen] = useState(false);
+    const toggle = () => setdropdownOpen(!dropdownOpen);
+    const [nameError, setnameError] = useState('')
+    const [mailError, setmailError] = useState('')
+    const [noteError, setnoteError] = useState('')
 
-  return (
-    <div className="App-Submit row" >
-        <div className='note col-md-5'>
-            <div className='hP'>
-                You got cool Projects you'd like to put up for the world to see, and it makes use glad. <br/>
-                Anyone can make a submission, but, to establish good value, check that: <br/> <br/>
-                <p>
-                    Your Project is not copywritten/copied/a plagiarised content <br/>  <br/>
-                    The document is well formatted and not a draft <br/> <br/>
-                    You proof read the document... you don't want to author typos<br/> <br/>
-                </p>
+    return (
+        <div className="App-Submit row" >
+            <div className='note col-md-5'>
+                <div className='hP'>
+                    You got cool Projects you'd like to put up for the world to see, and it makes use glad. <br />
+                    Anyone can make a submission, but, to establish good value, check that: <br /> <br />
+                    <p>
+                        Your Project is not copywritten/copied/a plagiarised content <br />  <br />
+                        The document is well formatted and not a draft <br /> <br />
+                        You proof read the document... you don't want to author typos<br /> <br />
+                    </p>
+                </div>
             </div>
-        </div>
-        <div className='forms col-md-7'>
-            <Form action={api} method='POST' enctype="multipart/form-data" target='error'>
-                <FormGroup className='card grp'>
-                <Label>You</Label> <span class='errSpan'>{nameError}</span>
-                    <Input type="text" name="firstName" placeholder="First Name *" onChange={(e)=>{
-                        setFirstName(e.target.value);
-                        if (e.target.value.length < 2){
-                            setnameError('Enter a valid name')
-                        }
-                        else{
-                            setnameError('')
-                            sethasErrorName(false)
-                        }
+            <div className='forms col-md-7'>
+                <Form action={api} method='POST' enctype="multipart/form-data" target='error'>
+                    <FormGroup className='card grp'>
+                        <Label>You</Label> <span class='errSpan'>{nameError}</span>
+                        <Input type="text" name="firstName" placeholder="First Name *" onChange={(e) => {
+                            setFirstName(e.target.value);
+                            if (e.target.value.length < 2) {
+                                setnameError('Enter a valid name')
+                            }
+                            else {
+                                setnameError('')
+                                sethasErrorName(false)
+                            }
                         }} />
-                    <div className='row'>
-                        <div className='col-md-6'>
-                            <Input name="lastName" type='text' placeholder="Last Name" onChange={(e)=>{
-                                setLastName(e.target.value)}} />
+                        <div className='row'>
+                            <div className='col-md-6'>
+                                <Input name="lastName" type='text' placeholder="Last Name" onChange={(e) => {
+                                    setLastName(e.target.value)
+                                }} />
+                            </div>
+                            <div className='col-md-6'>
+                                <Input name="otherName" type="text" placeholder="Other Name" onChange={(e) => { setOtherName(e.target.value) }} />
+                            </div>
                         </div>
-                        <div className='col-md-6'>
-                            <Input name="otherName" type="text" placeholder="Other Name" onChange={(e)=>{setOtherName(e.target.value)}} />
-                        </div>
-                    </div>
-                    <span class='errSpan'>{mailError}</span>
-                    <Input type="email" name="email" placeholder="Email Address *" onChange={(e)=>{
-                        setMail(e.target.value)
-                        var re = /\S+@\S+\.\S+/;
-                        if (re.test(e.target.value)){
-                            setmailError('')
-                            sethasErrorMail(false)
-                        }
-                        else{
-                            setmailError('Enter a valid mail')
-                        }
+                        <span class='errSpan'>{mailError}</span>
+                        <Input type="email" name="email" placeholder="Email Address *" onChange={(e) => {
+                            setMail(e.target.value)
+                            var re = /\S+@\S+\.\S+/;
+                            if (re.test(e.target.value)) {
+                                setmailError('')
+                                sethasErrorMail(false)
+                            }
+                            else {
+                                setmailError('Enter a valid mail')
+                            }
                         }} />
-                    <span>You'll get a mail notification after the review process</span>
-                </FormGroup>
-                <FormGroup className='card grp'>
-                <Label>The Project</Label>
-                    <Dropdown isOpen={dropdownOpen} toggle={toggle} name='type'>
-                        <DropdownToggle color='' type='select' style={{width:'100%', borderWidth:'1px', textAlign:'left', borderColor:'#909090', marginBottom:'1vw', marginTop:'1vw' }}>
-                            {docType}
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem onClick={()=>{setdocType('Undergraduate Project')}}>Undergraduate Project</DropdownItem>
-                            <DropdownItem onClick={()=>{setdocType('Graduate Project')}}>Graduate Project</DropdownItem>
-                            <DropdownItem onClick={()=>{setdocType('Term Paper')}}>Term Paper</DropdownItem>
-                            <DropdownItem onClick={()=>{setdocType('Independent Research')}}>Independent Research</DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
-                    <span class='errSpan'>{noteError}</span>
-                    <textarea name='note' placeholder="* Tell us a bit about the document..." onChange={(e)=>{
-                        setNote(e.target.value)
-                        if (e.target.value.length < 70){
-                            setnoteError(e.target.value.length.toString()+'/70 (minimum character)')
-                        }
-                        else{
-                            setnoteError('')
-                            sethasErrorNote(false)
-                        }
+                        <span>You'll get a mail notification after the review process</span>
+                    </FormGroup>
+                    <FormGroup className='card grp'>
+                        <Label>The Project</Label>
+                        <Dropdown isOpen={dropdownOpen} toggle={toggle} name='type'>
+                            <DropdownToggle color='' type='select' style={{ width: '100%', borderWidth: '1px', textAlign: 'left', borderColor: '#909090', marginBottom: '1vw', marginTop: '1vw' }}>
+                                {docType}
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem onClick={() => { setdocType('Undergraduate Project') }}>Undergraduate Project</DropdownItem>
+                                <DropdownItem onClick={() => { setdocType('Graduate Project') }}>Graduate Project</DropdownItem>
+                                <DropdownItem onClick={() => { setdocType('Term Paper') }}>Term Paper</DropdownItem>
+                                <DropdownItem onClick={() => { setdocType('Independent Research') }}>Independent Research</DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                        <span class='errSpan'>{noteError}</span>
+                        <textarea name='note' placeholder="* Tell us a bit about the document..." onChange={(e) => {
+                            setNote(e.target.value)
+                            if (e.target.value.length < 70) {
+                                setnoteError(e.target.value.length.toString() + '/70 (minimum character)')
+                            }
+                            else {
+                                setnoteError('')
+                                sethasErrorNote(false)
+                            }
                         }} />
                         <Label></Label>
-                    <span>Browse or drag file (pdf format only)</span>
+                        <span>Browse or drag file (pdf format only)</span>
                         <Label for='file-upload' className='upload'>
                             {selectFileName}
-                            <input type='file' name='fupload' id='file-upload' accept="application/pdf" onChange={(e)=>{
-                                setSelectFileName(e.target.value.replace('C:\\fakepath\\',''))
-                                sethasErrorFile(false)}}/>
+                            <input type='file' name='fupload' id='file-upload' accept="application/pdf" onChange={(e) => {
+                                setSelectFileName(e.target.value.replace('C:\\fakepath\\', ''))
+                                sethasErrorFile(false)
+                            }} />
                         </Label>
-                </FormGroup>
-                <div className='card grp' >
-                    <FormGroup check>
-                        <Label check><input type='checkbox' onChange={(e)=>{ setDisabled(!e.target.checked) }} /> &nbsp; Accept Privacy Policy and Terms of Service </Label>
                     </FormGroup>
-                </div>
-                <span class='errSpan'>{fillAll}</span>
+                    <div className='card grp' >
+                        <FormGroup check>
+                            <Label check><input type='checkbox' onChange={(e) => { setDisabled(!e.target.checked) }} /> &nbsp; Accept Privacy Policy and Terms of Service </Label>
+                        </FormGroup>
+                    </div>
+                    <span class='errSpan'>{fillAll}</span>
 
-                <div className='col'>
-                <Label> {' '} </Label>
-                <Button className='sendBtn' id='submitBtn' type='submit' disabled={disabled} onClick={sendSubmission} > Submit </Button>
-                </div>
+                    <div className='col'>
+                        <Label> {' '} </Label>
+                        <Button className='sendBtn' id='submitBtn' type='submit' disabled={disabled} onClick={sendSubmission} > Submit </Button>
+                    </div>
 
-                <Modal isOpen={modalOpen} backdrop={false} toggle={toggleModal}>
-                    <ModalHeader>Loading</ModalHeader>
-                    <ModalBody>
-                        {Loading}
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color='outline' onClick={toggleModal}>Close</Button>
-                    </ModalFooter>
-                </Modal>
-            </Form>
+                    <Modal isOpen={modalOpen} backdrop={false} toggle={toggleModal}>
+                        <ModalHeader>Loading</ModalHeader>
+                        <ModalBody>
+                            {Loading}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color='outline' onClick={toggleModal}>Close</Button>
+                        </ModalFooter>
+                    </Modal>
+                </Form>
                 <iframe title='Upload' name='error' hidden height='0px'>
                 </iframe>
+            </div>
+            <StickyBanner
+                zoneId="4420476"
+                horizontalPosition="left"
+                verticalPosition="bottom"
+                format="728x90"
+            />
         </div>
-      <StickyBanner
-        zoneId="4420476"
-        horizontalPosition="left"
-        verticalPosition="bottom"
-        format="728x90"
-      />
-    </div>
-  );
+    );
 }
 
 export default Submit;
